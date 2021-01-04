00ed9cdd2245712b6ee9b95cdd9b330aa1841061
RahimjonPc <abduganievrahimjon@gmail.com>
2021-01-05 00:08:06 +0500

    smth

---
+++ JWT/Employee/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Employee,Department
+from .serializers import EmployeeSerializer,DepartmentSerializer
+
+# Create your views here.
+
+class EmployeeView(viewsets.ModelViewSet):
+    queryset=Employee.objects.all()
+    serializer_class=EmployeeSerializer
+
+class DepartmentView(viewsets.ModelViewSet):
+    queryset=Department.objects.all()
+    serializer_class=DepartmentSerializer
